408d4b55898b5cf44ffff13138155a5459c09620
Fiona-091305 <349460105@gapps.yrdsb.ca>
2026-05-20 11:02:53 -0400

    changed the name

--- Eco.java
+++ Eco.java
@@ -1,15 +1,15 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class We here.
+ * Write a description of class Eco here.
  * 
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class We extends Actor
+public class Eco extends Actor
 {
     /**
-     * Act - do whatever the We wants to do. This method is called whenever
+     * Act - do whatever the Eco wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act()

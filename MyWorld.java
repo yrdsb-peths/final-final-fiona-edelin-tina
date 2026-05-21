8081aa66337dafa87895a6d59ef65f822e2b103c
edelinliu <440009774@gapps.yrdsb.ca>
2026-05-21 11:29:28 -0400

    Update MyWorld.java

--- MyWorld.java
+++ MyWorld.java
@@ -19,6 +19,15 @@ public class MyWorld extends World {
     }
     
     /**
+     * End the game and draw 'GameOver'
+     */
+    public void gameOver()
+    {
+        Label gameOverLabel = new Label("Game Over", 100);
+        addObject(gameOverLabel, 300, 200);
+    }
+    
+    /**
      * Increase score
      */
     public void increaseScore()

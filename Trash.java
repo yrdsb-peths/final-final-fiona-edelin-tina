2ad447d415b39c8c054eb83b5e51d1346153c06f
Fiona-091305 <349460105@gapps.yrdsb.ca>
2026-05-21 18:03:54 -0400

    not fixxed for the image setter but ill fix it later

--- Trash.java
+++ Trash.java
@@ -12,12 +12,21 @@ public class Trash extends Actor
      * Act - do whatever the Trash wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    GreenfootImage[] trashImages = new GreenfootImage[5];
+
     public Trash()
     {
-        GreenfootImage image = new GreenfootImage("trash.png");
-        image.scale(35, 40);
-        setImage(image);
+        for(int i = 0; i < trashImages.length; i++)
+        {
+            trashImages[i] = new GreenfootImage("trash" + i + ".png");
+            trashImages[i].scale(35, 40);
+        }
+
+        int randomImage = Greenfoot.getRandomNumber(trashImages.length);
+
+        setImage(trashImages[randomImage]);
     }
+
     public void act()
     {
         // Add your action code here.

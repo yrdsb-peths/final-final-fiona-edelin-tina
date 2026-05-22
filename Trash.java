836cdcaa38cada8d936f679403027ba178150d1f
Fiona-091305 <349460105@gapps.yrdsb.ca>
2026-05-22 11:31:55 -0400

    fixde the randomizer for images

--- Trash.java
+++ Trash.java
@@ -12,18 +12,18 @@ public class Trash extends Actor
      * Act - do whatever the Trash wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
-    GreenfootImage[] trashImages = new GreenfootImage[5];
+    private GreenfootImage[] trashImages = new GreenfootImage[4];
 
     public Trash()
     {
         for(int i = 0; i < trashImages.length; i++)
         {
-            trashImages[i] = new GreenfootImage("trash" + i + ".png");
+            trashImages[i] = new GreenfootImage("trash/trash" + (i + 1) + ".png");
             trashImages[i].scale(35, 40);
         }
-
+    
         int randomImage = Greenfoot.getRandomNumber(trashImages.length);
-
+    
         setImage(trashImages[randomImage]);
     }
 

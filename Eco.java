333052d5f81bd93b512bfb5f22bee2ea542a47a8
TinaGaohub <440020959@gapps.yrdsb.ca>
2026-05-22 15:29:10 -0400

    Added character animation

--- Eco.java
+++ Eco.java
@@ -12,30 +12,77 @@ public class Eco extends Actor
      * Act - do whatever the Eco wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    GreenfootImage[] front = new GreenfootImage[3];
+    GreenfootImage[] back = new GreenfootImage[2];
+    GreenfootImage[] right = new GreenfootImage[3];
+    GreenfootImage[] left = new GreenfootImage[4];
+    GreenfootImage[] clean = new GreenfootImage[2];
+    String facing = "front";
+    SimpleTimer animationTimer = new SimpleTimer();
+    int frame = 0;
+    
+    boolean cleaning = false;
+    boolean moving = false;
+    public Eco()
+    {
+        for(int i = 0; i < front.length; i++)
+        {
+            front[i] = new GreenfootImage("images/person/front" + i + ".png");
+            front[i].scale(50,80);
+        }
+        for(int i = 0; i < back.length; i++)
+        {
+            back[i] = new GreenfootImage("images/person/back" + i + ".png");
+            back[i].scale(50,80);
+        }
+        for(int i = 0; i < left.length; i++)
+        {
+            left[i] = new GreenfootImage("images/person/left" + i + ".png");
+            left[i].scale(80,80);
+        }
+        for(int i = 0; i < right.length; i++)
+        {
+            right[i] = new GreenfootImage("images/person/right" + i + ".png");
+            right[i].scale(80,80);
+        }
+        for(int i = 0; i < clean.length; i++)
+        {
+            clean[i] = new GreenfootImage("images/person/clean" + i + ".png");
+            clean[i].scale(50,80);
+        }
+        setImage(front[0]);
+        animationTimer.mark();
+    }
     public void act()
     {
         // Add your action code here.
-        setImage("person.png");
-        GreenfootImage image = getImage();
-        image.scale(50, 65);
-        setImage(image);
+        moving = false;
         collect();
-        if(Greenfoot.isKeyDown("left"))
+        if(Greenfoot.isKeyDown("left") && !cleaning)
         {
             move(-2);
+            facing = "left";
+            moving = true;
         }
-        else if(Greenfoot.isKeyDown("right"))
+        else if(Greenfoot.isKeyDown("right") && !cleaning)
         {
             move(2);
+            facing = "right";
+            moving = true;
         }
-        if(Greenfoot.isKeyDown("up"))
+        if(Greenfoot.isKeyDown("up") && !cleaning)
         {
             setLocation(getX(), getY() - 2);
+            facing = "back";
+            moving = true;
         }
-        if(Greenfoot.isKeyDown("down"))
+        if(Greenfoot.isKeyDown("down") && !cleaning)
         {
             setLocation(getX(), getY() + 2);
+            facing = "front";
+            moving = true;
         }
+        animation();
     }
     //remove the trash
     public void collect()
@@ -46,6 +93,56 @@ public class Eco extends Actor
             MyWorld world = (MyWorld)getWorld();
             world.createTrash();
             world.increaseScore();
+            
+            facing = "clean";
+            cleaning = true;
+            
+            imageIndex = 0;
+        }
+    }
+    int imageIndex = 0;
+    public void animation()
+    {
+        imageIndex++;
+        if(!moving && !cleaning)
+        {
+            setImage(front[0]);
+            return;
+        }
+        if(animationTimer.millisElapsed() < 50)
+        {
+            return;
+        }
+        animationTimer.mark();
+        
+        if(facing.equals("front"))
+        {
+            setImage(front[imageIndex % front.length]);
+        }
+        else if(facing.equals("back"))
+        {
+            setImage(back[imageIndex % back.length]);
+        }
+        if(facing.equals("right"))
+        {
+            setImage(right[imageIndex % right.length]);
+        }
+        if(facing.equals("left"))
+        {
+            setImage(left[imageIndex % left.length]);
+        }
+        if(facing.equals("clean"))
+        {
+            setImage(clean[imageIndex % clean.length]);
+            imageIndex++;
+            
+            if(imageIndex >= clean.length)
+            {
+                cleaning = false;
+                facing = "front";
+                imageIndex = 0;
+            }
         }
     }
 }
+

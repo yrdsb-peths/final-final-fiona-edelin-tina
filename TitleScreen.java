1dd0357e0dd870f7fea752888a68b5dea6607743
edelinliu <440009774@gapps.yrdsb.ca>
2026-05-24 14:27:48 -0400

    Update TitleScreen.java

--- TitleScreen.java
+++ TitleScreen.java
@@ -8,7 +8,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class TitleScreen extends World
 {
-    Label titleLabel = new Label("Operation Cleanup", 60);
+    Label titleLabel = new Label("Operation Cleanup", 50);
     /**
      * Constructor for objects of class TitleScreen.
      * 
@@ -17,9 +17,10 @@ public class TitleScreen extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
-        addObject(titleLabel, getWidth()/2, getHeight()/2);
+        addObject(titleLabel, 250, 80);
+        prepare();
     }
-    
+
     /**
      * The main world act loop
      */
@@ -31,5 +32,19 @@ public class TitleScreen extends World
             MyWorld gameWorld = new MyWorld();
             Greenfoot.setWorld(gameWorld);
         }
+    }
+    
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
+        Eco eco = new Eco();
+        addObject(eco,504,204);
+        eco.setLocation(530,104);
+        eco.setLocation(520,95);
+        eco.setLocation(513,75);
+        eco.setLocation(513,76);
     }
 }

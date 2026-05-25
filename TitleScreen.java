295b81cbe176f15d0ec6a29b5908e061a8832dd4
edelinliu <440009774@gapps.yrdsb.ca>
2026-05-25 11:16:00 -0400

    Update TitleScreen.java

--- TitleScreen.java
+++ TitleScreen.java
@@ -17,6 +17,7 @@ public class TitleScreen extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
+        setBackground("background.jpg");
         addObject(titleLabel, 250, 80);
         prepare();
     }
